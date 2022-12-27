f1de87b32874d4976fe2d53a824728521aeb3c04
Jeyson Junior Ventura Aguilar <jeysonv.sis@gmail.com>
2022-12-27 12:24:31 -0500

    ciclos de vida ngDoCheck ngOnChanges

--- src/app/modules/task/components/card/card.component.ts
+++ src/app/modules/task/components/card/card.component.ts
@@ -1,4 +1,4 @@
-import { AfterViewChecked, AfterViewInit, Component, ElementRef, Input,OnInit,Renderer2,ViewChild } from '@angular/core';
+import { AfterViewChecked, AfterViewInit, Component, DoCheck, ElementRef, Input,OnChanges,OnInit,Renderer2,SimpleChanges,ViewChild } from '@angular/core';
 import { interval } from 'rxjs';
 
 @Component({
@@ -6,7 +6,7 @@ import { interval } from 'rxjs';
   templateUrl: './card.component.html',
   styleUrls: ['./card.component.css'],
 })
-export class CardComponent implements OnInit, AfterViewInit, AfterViewChecked {
+export class CardComponent implements OnInit, AfterViewInit, AfterViewChecked, DoCheck, OnChanges {
 
   @ViewChild('refId') elementRefId:ElementRef = new ElementRef('inicializado');
 
@@ -16,23 +16,34 @@ export class CardComponent implements OnInit, AfterViewInit, AfterViewChecked {
   constructor(private render2:Renderer2) { }
 
   ngOnInit(): void {
-    console.log('Holaa soy un Card Creado!! 😉', this.elementRefId);
-    interval(1000).subscribe((evento) => {
-      this.idOrder = Date.now();
-    });
+    // console.log('Holaa soy un Card Creado!! 😉', this.elementRefId);
+    // interval(1000).subscribe((subscription) => {
+    //   this.idOrder = Date.now();
+    // })
   }
 
   //cargado cuando la vista esta cargada / se usa para aplicar estilos o comportamientos de manera dinamica al terminar la carga
   ngAfterViewInit(): void {
-    console.log('Holaa soy un Card Creado!! 😉', this.elementRefId);
+    // console.log('Holaa soy un Card Creado!! 😉', this.elementRefId);
     const elementTitle = this.elementRefId.nativeElement;
     this.render2.setStyle(elementTitle, 'color', 'red');
   }
 
   //cuando la vista sufra un cambio el HTML / animaciones / librerias externas
   ngAfterViewChecked(): void {
-    console.log('Holaa soy un Card Modificado!! 😉', this.elementRefId);
+    // console.log('Holaa soy un Card Modificado!! 😉', this.elementRefId);
   }
+
+  //Cambios en los atributos del componente y de los hijos
+  ngDoCheck(): void {
+    // console.log('Holaa soy el Docheck!! 😉');
+  }
+
+  //Se ejecuta solo cuando vienen los datos del padre
+  ngOnChanges(changes: SimpleChanges): void {
+    console.log(changes);
+  }
+
 
   sendData():void{
   }
